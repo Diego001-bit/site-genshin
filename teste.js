659dfdd6260221c21c140ef629e6ddeda8f6b22d
Digamer001 <diegobezerra864@gmail.com>
2023-05-12 15:00:02 -0300

    deu certo

---
+++ teste.js
@@ -0,0 +1,8 @@
+var shogun = 1240
+if (shogun >= 2000){
+    console.log(`Esta exelente`)
+}else if (shogun >= 1500 && shogun <=2000){
+    console.log(`Esta bom,mas pode trazer dificuldades no abismo`)
+}else{
+    console.log(`Mesmo sendo um jogo facil,tera grandes dificuldades nos chefes semanais entre outras atividades diarias `)
+}
